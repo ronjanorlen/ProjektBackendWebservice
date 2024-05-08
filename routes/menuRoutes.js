638b6f471b79df60d864eb82a6ba23cb1928b9cc
Ronja Norlén <rono2300@student.miun.se>
2024-05-08 14:26:45 +0200

    Routes för att uppdatera och ta bort maträtt

--- routes/menuRoutes.js
+++ routes/menuRoutes.js
@@ -36,9 +36,46 @@ router.post("/", authenticateToken, async (req, res) => {
         res.status(201).json({ message: "Maträtt tillagd" });
         // Om eventuella fel
     } catch (error) {
-        console.error("Något gick fel vid tillägg av maträtt ", error);
+        console.error("Något gick fel vid tillägg av maträtt: ", error);
         res.status(500).json(error);
     }
-})
+});
+
+// PUT-anrop för att uppdatera maträtt
+router.put("/:id", authenticateToken, async (req, res) => {
+    try {
+        const id = req.params.id; // Hämta id på maträtt
+        // Hitta maträtt med angivet id och uppdatera med uppgifterna från bodyn
+        const updateMeal = await Menu.findByIdAndUpdate(id, req.body, {new: true});
+        // Om maträtten inte hittas, returnera felmeddelande
+        if (!updateMeal) {
+            return res.status(404).json({ message: "Maträtt hittades inte" });
+        }
+        // Om maträtt uppdateras korrekt
+        return res.json({ message: "Maträtt uppdaterad", updateMeal });
+        // Om eventuella fel
+    } catch (error) {
+        console.error("Något gick fel vid uppdatering av maträtt: ", error);
+        return res.status(400).json(error);
+    }
+});
+
+router.delete("/:id", authenticateToken, async (req, res) => {
+    try {
+        const id = req.params.id; // Hämta id på maträtt
+        // Hitta maträtt och ta bort
+        const deleteMeal = await Menu.findByIdAndDelete(id);
+        // Om maträtt inte hittas, returnera felmeddelande
+        if (!deleteMeal) {
+            return res.status(404).json({ message: "Maträtt hittades inte" });
+        }
+        // Om maträtt tas bort korrekt
+        return res.json({ message: "Maträtt borttagen" });
+        // Om eventuella fel
+    } catch (error) {
+        console.error("Något gick fel vid borttagning av maträtt: ", error);
+        return res.status(500).json(error);
+    }
+});
 
 module.exports = router; // Exporta Router-objekt
